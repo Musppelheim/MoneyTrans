1650b92bb38a9ad0feb031c6f4383b835cc9e9b1
Musppelheim <neloliver@hotmail.com>
2020-02-19 08:43:25 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = (int) 2.E9;
+        int transfers = (int) 5.E8;
+        int total = account - transfers;
+        System.out.println(total);
+    }
+}
