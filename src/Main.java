ea1b383c6f8fc5ad832ea1c3e2ccd40be79f33cc
Musppelheim <neloliver@hotmail.com>
2020-02-21 03:41:46 +0700

    added new

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int account = (int) 2.E9;
-        int transfers = (int) 5.E8;
-        int total = account - transfers;
+        int account = -2_000_000_000;
+        int transfers = -500_000_000;
+        int total = account + transfers;
         System.out.println(total);
     }
 }
