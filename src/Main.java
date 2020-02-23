25cd321705d0e689398b416eebe020dfa59ec2e9
Musppelheim <neloliver@hotmail.com>
2020-02-23 17:05:22 +0700

    fixed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int account = -2_000_000_000;
-        int transfers = -500_000_000;
+        int account = 2_000_000_000;
+        int transfers = 500_000_000;
         int total = account + transfers;
         System.out.println(total);
     }
